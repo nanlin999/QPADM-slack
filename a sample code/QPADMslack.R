e67bf64ca260c7b2d0eee8129f34c5b4fdc2f89f
Ye Fan <49698544+fansuye@users.noreply.github.com>
2019-07-17 20:27:28 +0800

    Update QPADMslack.R

--- a sample code/QPADMslack.R
+++ a sample code/QPADMslack.R
@@ -1,15 +1,16 @@
 # Penalized quantile regression Using the Slack Variable Representation. This is a non-parallel implementation in R.
 # The QPADMslack() function is implemented using Rcpp. To use the code, we should install the R package, "Rcpp", first.
-# QPADMslack(eps, y, x, tau, lambda, a, pho, funname, maxstep). 
-# "eps" The terminate condition
+# QPADMslackCPP(y, x, tau, lambda, a, pho, funname, maxstep, eps, intercept). 
 # "y" The response vector
-# "x" The design matrix (with intercept)
+# "x" The design matrix (without intercept)
 # "tau" The quantile of interest
 # "lambda" The penalization parameter
 # "a" The shape parameter for the SCAD/MCP penalty
 # "pho" The augmentation parameter for the ADMM
-# "funname" Name of the penalty to use, currently support ("scad","mcp")
+# "funname" Name of the penalty to use, currently support ("scad", "mcp")
 # "maxstep" Maximum number of iterations allowed
+# "eps" The terminate condition
+# "intercept" Whether to include the intercept into the model
 # value: The coefficient estimation of the linear quantile regression model, the number of iterations and the running time
 n = 30000 
 p = 1000   
@@ -20,7 +21,7 @@ X = X%*%chol(cov)
 X[,1] = pnorm(X[,1])
 e = rnorm(n)
 Y = X[,6]+X[,12]+X[,15]+X[,20]+0.7*X[,1]*e
-QPADMslack_scad = QPADMslack(1e-03, Y, X, 0.7, 15, 3.7, 10, "scad", 500, 5e-02)
-beta_qadmm_scad = QPADMslack_scad[1:p]
+QPADMslack_scad = QPADMslackCPP(Y, X, 0.7, 15, 3.7, 5, "scad", 500, 1e-03, FALSE)
+beta = QPADMslack_scad[1:p]
 step = QPADMslack_scad[p+1]
 time = QPADMslack_scad[p+2]
